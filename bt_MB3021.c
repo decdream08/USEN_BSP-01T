1bf81ef9a46eda71ba9764cb457d0520a954df0c
decdream08 <decdream@naver.com>
2023-04-25 11:44:24 +0900

    MCU Version 정보 업데이트

--- bt_MB3021.c
+++ bt_MB3021.c
@@ -377,7 +377,7 @@ typedef enum {
 
 //Variable
 #ifdef VERSION_INFORMATION_SUPPORT
-char MCU_Version[6] = "230418"; //MCU Version Info
+char MCU_Version[6] = "230425"; //MCU Version Info
 #ifdef SPP_EXTENSION_V50_ENABLE
 char BT_Version[7]; //MCU Version Info
 #endif
